99a9ec3ad5cbd9bdc9e9297c4a04ef1668b800f7
rmelchorv <rmelchorv@gmail.com>
2022-10-26 09:13:34 -0500

    Part1 a. React

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="world!" age="all"/>
+      <Hello name="class." age={40+3}/>
+    </div>
+  )}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +23,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
