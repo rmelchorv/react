fc4a667b19ec9ad37130033d4e4a338141e05f0e
rmelchorv <rmelchorv@gmail.com>
2022-10-26 16:52:04 -0500

    part1. c. Pasando el estado a componentes hijos

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
+const Display = ({ counter }) => {
+  return (
+    <>
+      <span>{counter} </span>
+    </>
+  )
+}
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
 const App = () => {
   const [ counter, setCounter ] = useState(0)
   const increaseBy1 = () => setCounter(counter + 1)
+  const decreaseBy1 = () => setCounter(counter - 1)
   const set2Zero = () => setCounter(0)
 
   return (
     <div>
-      <span>{counter} </span>
-      <button onClick={increaseBy1}>plus</button>
-      <button onClick={set2Zero}>zero</button>
+      <Display counter={counter} />
+      <Button handleClick={increaseBy1} text="plus" />
+      <Button handleClick={decreaseBy1} text="minus" />
+      <Button handleClick={set2Zero} text="zero" />
     </div>
   )
 }
