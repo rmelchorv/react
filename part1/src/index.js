93264eb61ee6776da7d548526d52817f6ee7fa15
rmelchorv <rmelchorv@gmail.com>
2022-10-27 18:19:39 -0500

    part1. d. Renderizado condicional

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client';
 
 const Display = ({ counter }) => <><span> {counter} </span></>
 const Button = ({ handleClick, text }) => <><button onClick={handleClick}>{text}</button></>
-const History = ({ allClics }) => <><p>{allClics.join('-')}</p></>
+const History = ({ allClics }) => {
+  if (allClics.length === 0) {
+    return (
+      <><p>the app is used by pressing the buttons</p></>
+    )
+  }
+  return (  
+    <><p>{allClics.join('-')}</p></>
+  )
+}
 const App = () => {
   const [clicks, setClicks] = useState({ left: 0, right: 0, all: [] })
   const increaseLeft = () => setClicks({ ...clicks, left: clicks.left + 1, all: clicks.all.concat('L') })
