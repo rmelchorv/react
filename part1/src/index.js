a46bca5bf27d1f209347ecf5edec4fea1b5375f8
rmelchorv <rmelchorv@gmail.com>
2022-10-31 16:26:54 -0600

    part1. d. constFunción que devuelve una función (e)

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,16 +4,22 @@ import ReactDOM from 'react-dom/client';
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const setToValue = (newValue) => () => {
-      setValue(newValue)
-    }
+  const setToValue = (newValue) => {
+    setValue(newValue)
+  }
 
   return (
     <div>
-      {value}&nbsp;
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>zero</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
     </div>
   )
 }
