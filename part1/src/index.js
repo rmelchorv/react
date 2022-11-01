fcf8eb4678aa03107b1c9808fb4cc32e8f8436e2
rmelchorv <rmelchorv@gmail.com>
2022-10-31 22:02:10 -0600

    Ejercicio 1.9 - unicafe, paso 4 (b)

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,8 +2,26 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const Title = ({ title }) => <><h1>{title}</h1></>
-const Statistic = ({ text, value }) => <div>{text}: {value}</div>
 const Button = ({ handleClick, text }) => <><button onClick={handleClick}>{text}</button></>
+const Statistics = ({ feedback }) => {
+  // statistics
+  const all = (feedback) => feedback.good + feedback.neutral + feedback.bad
+  const average = (feedback) => ((feedback.good - feedback.bad) / all(feedback)).toFixed(2) + " %"
+  const positive = (feedback) => (feedback.good * 100.0 / all(feedback)).toFixed(2) + " %"
+
+  if (all(feedback) === 0)
+    return "no feedback given"
+  return (
+    <>
+      <div>good: {feedback.good}</div>
+      <div>neutral: {feedback.neutral}</div>
+      <div>bad: {feedback.bad}</div>
+      <div>all: {all(feedback)}</div>
+      <div>average: {average(feedback)}</div>
+      <div>positive: {positive(feedback)}</div>
+    </>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   // ... the state of the application should remain in the App root component
@@ -11,20 +29,6 @@ const App = () => {
   const increaseGood = () => setFeedback({ ...feedback, good: feedback.good + 1 })
   const increaseNeutral = () => setFeedback({ ...feedback, neutral: feedback.neutral + 1 })
   const increaseBad = () => setFeedback({ ...feedback, bad: feedback.bad + 1 })
-  // statistics
-  const all = (feedback) => feedback.good + feedback.neutral + feedback.bad
-  const average = (feedback) => {
-    const _all = all(feedback)
-    if (_all === 0)
-      return "0.00 %"
-    return ((feedback.good - feedback.bad) / _all).toFixed(2) + " %"
-  }
-  const positive = (feedback) => {
-    const _all = all(feedback)
-    if (_all === 0)
-      return "0.00 %"
-    return (feedback.good * 100.0 / _all).toFixed(2) + " %"
-  }
   // factory for event handlers
   const setToFeedback = (value) => value
 
@@ -35,12 +39,7 @@ const App = () => {
       <Button handleClick={setToFeedback(increaseNeutral)} text="neutral"></Button>
       <Button handleClick={setToFeedback(increaseBad)} text="bad"></Button>
       <Title title="statistics" />
-      <Statistic text="good" value={feedback.good} />
-      <Statistic text="neutral" value={feedback.neutral} />
-      <Statistic text="bad" value={feedback.bad} />
-      <Statistic text="all" value={all(feedback)} />
-      <Statistic text="average" value={average(feedback)} />
-      <Statistic text="positive" value={positive(feedback)} />
+      <Statistics feedback={feedback} />
     </div>
   )
 }
