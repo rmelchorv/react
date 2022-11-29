07a774fac3cf3fcf99d54373952f1f9d7866880a
rmelchorv <rmelchorv@gmail.com>
2022-11-29 16:16:14 -0600

    part3. a. Simple web server

--- part3/contenido/index.js
+++ part3/contenido/index.js
@@ -1,1 +1,31 @@
-console.log("Hello world!")
+const http = require("http");
+const PORT = 3001;
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json;charset=UTF8" });
+  res.end(JSON.stringify(notes));
+});
+
+app.listen(PORT);
+console.log(`Server running on http://localhost:${PORT}`);
